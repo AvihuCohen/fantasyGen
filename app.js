be54965d767d3ccba8c1d718d7a3542b03a481e8
AvihuCohen <cohenavihu1@gmail.com>
2019-09-26 18:31:14 +0300

    add env.port to listen

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use("/players", playersRoute);
 app.use("/team", teamRoute);
 
 // process.env.PORT, process.env.IP
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Server is running.");
 });
 
